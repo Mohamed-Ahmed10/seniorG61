b956bcb7c7835b28d1c0f44f16dca425ef489af5
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2023-01-30 20:43:14 +0200

    first session

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import myLogo from "./logo.svg";
+import "./App.css";
+import {SecondNav} from "./components/Navbar";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <div className="App">
-      Hello react
-    </div>
-  );
+    var myName = "Mohamed Ahmed";
+    return (
+        <>
+            <Navbar />
+            <SecondNav />
+            <div className="App">
+                Hello react
+                <img src={myLogo} alt="test" width={200} />
+                <div>{myName}</div>
+                <input type="text" />
+            </div>
+            <span></span>
+            <Header />
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+const Navbar = () => {
+    return <nav>This is nav</nav>;
+};
+const SecondNav = () => {
+    return <div>this is second one</div>;
+};
+export default Navbar;
+export {SecondNav};
